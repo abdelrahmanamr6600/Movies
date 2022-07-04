cbbe99a337ca8bbde1c0c558a95de807b71c9f74
MAHMOUD <khattabm719@gmail.com>
2022-07-04 13:35:49 +0200

    a

--- app/src/main/java/com/example/movies/Register/RegisterActivity.kt
+++ app/src/main/java/com/example/movies/Register/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.movies.Register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movies.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
