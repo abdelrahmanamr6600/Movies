00a84d6645778349304d0d517256fe39b009fc29
MAHMOUD <khattabm719@gmail.com>
2022-07-05 03:17:02 +0200

    بقولك ا نا ضيفت الحاجات اهي ولسه هظبط الكنترول بينهم تمام ؟ انا عاوزك بس فى الbottom nav تظبطلي الicons تحط icons لطيفه كدا على زوقك

---
+++ app/src/main/java/com/example/movies/MainUser/HomeUser.kt
@@ -0,0 +1,12 @@
+package com.example.movies.MainUser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movies.R
+
+class HomeUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_user)
+    }
+}
