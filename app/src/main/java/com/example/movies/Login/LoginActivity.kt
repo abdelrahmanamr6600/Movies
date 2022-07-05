7b8e48e5f7f8e397b8a06acf75a7bad4b24efb24
Mahmoud Ahmed <maahmed486@gmail.com>
2022-07-05 16:59:02 +0200

    login*Register

--- app/src/main/java/com/example/movies/Login/LoginActivity.kt
+++ app/src/main/java/com/example/movies/Login/LoginActivity.kt
@@ -1,12 +1,59 @@
 package com.example.movies.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.movies.R
+import android.widget.Toast
+import com.example.movies.MainActivity
+import com.example.movies.Register.RegisterActivity
+import com.example.movies.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginTvRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginBtnLog.setOnClickListener {
+            val email = binding.loginTvEmail.text.toString()
+            val password = binding.loginTvPassword.text.toString()
+
+            if ( email.isNotEmpty() &&password.isNotEmpty() ){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    else{
+                        Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this,"Pleas Filed the Text", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
+//هراجعها لسه بعد مااعمل رن عشان اشتاكد منهالانها فتحت البرنامج بعد الريجيستر من غير يا يدخل الاميل ويعمل لوج ان
+//    override fun onStart() {
+//        super.onStart()
+//        if (firebaseAuth.currentUser != null){
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 }
--- app/src/main/java/com/example/movies/Register/RegisterActivity.kt
+++ app/src/main/java/com/example/movies/Register/RegisterActivity.kt
@@ -1,12 +1,58 @@
 package com.example.movies.Register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.movies.R
+import android.widget.Toast
+import com.example.movies.Login.LoginActivity
+import com.example.movies.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.registerTvLogin.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.registerBtn.setOnClickListener {
+            val name = binding.registerTvName.text.toString()
+            val email = binding.registerTvEmail.text.toString()
+            val number = binding.registerTvPhone.text.toString()
+            val password = binding.registerTvPassword.text.toString()
+            val confPassword = binding.registerTvConfrimPassword.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && number.isNotEmpty() &&
+                password.isNotEmpty() && confPassword.isNotEmpty()){
+                if (password == confPassword){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        }
+                        else{
+                            Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(this, "Password is not mach ", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"Pleas Filed the Text", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
